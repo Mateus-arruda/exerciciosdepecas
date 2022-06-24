f86fb8fe4ab063fd5c48b83539b9ef88c2e3e452
Mateus Arruda <arrudamateus02@gmail.com>
2022-06-24 16:06:39 -0400

    exercicios de pecas

---
+++ let pesopeca = 200.js
@@ -0,0 +1,21 @@
+let pesopeca = 200
+let numeropecas = 20
+let nomepeca = "volante"
+
+if(pesopeca > 120) {
+    console.log("pode-se cadastrar a peca") 
+} else {
+    console.log("peca com peso insufuciente")
+} 
+
+if ((numeropecas >= 20)) {
+    console.log("impossivel cadastrar numero de pecas excedido")
+} else {
+    console.log("prossiga com o cadastro")
+}
+
+if (nomepeca.lenght < 4) {
+    console.log("numero de caracteres insuficiente")
+} else {
+    console.log("pode cadastrar")
+}
